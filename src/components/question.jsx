33b546f98ed82cdd9f0c0e2674566785d44b8c8f
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-12-12 11:51:22 -0500

    questions input mapped

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
 
 export default class Question extends Component {
 	render() {
+		const { id, evaluation } = this.props.question;
+
 		return (
-			<div>
-				<p>hello</p>
-			</div>
+			<Col>
+				<Form.Group>
+					<Form.Label htmlFor="score">{evaluation}</Form.Label>
+					<Form.Control id="score" name="score" type="number" min="0" max="5" />
+				</Form.Group>
+			</Col>
 		);
 	}
 }
--- src/components/questionnaire.jsx
+++ src/components/questionnaire.jsx
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import Question from '../components/question';
+import Question from './question';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstral/Button';
 
 export default class Questionnaire extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-            fullname: '',
-            email: '',
-            repo: '',
-            URL: '',
-            displayed: '',
-            emailSent: '',
+			fullname: '',
+			email: '',
+			repo: '',
+			URL: '',
+			displayed: '',
+			emailSent: '',
+			score: '',
 			questions: [
 				{
 					id: 0,
@@ -82,7 +85,27 @@ export default class Questionnaire extends Component {
 
 	makeQuestions = (questions) => {
 		return questions.map((question) => {
-			return <Question question={question} key={question.id} />;
+			return (
+				<Question question={question} key={question.id} value={this.state.score} onChange={this.handleChange} />
+			);
+		});
+	};
+
+	handleChange = (event) => {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		this.setState({
+			disabled: true
 		});
 	};
 
@@ -90,7 +113,52 @@ export default class Questionnaire extends Component {
 		return (
 			<Container>
 				<Row>
-					<Col>{this.makeQuestions(this.state.questions)}</Col>
+					<Form onSubmit={this.handleSubmit} style={{ padding: '1rem' }} className="lfont">
+						<Form.Group>
+							<Form.Label htmlFor="full-name">Name</Form.Label>
+							<Form.Control
+								id="full-name"
+								name="name"
+								type="text"
+								value={this.state.name}
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label htmlFor="email">Email</Form.Label>
+							<Form.Control
+								id="email"
+								name="email"
+								type="email"
+								value={this.state.email}
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+						<Form.Group>
+							<Form.Label htmlFor="message">Message</Form.Label>
+							<Form.Control
+								id="message"
+								name="message"
+								as="textarea"
+								rows="4"
+								value={this.state.message}
+								onChange={this.handleChange}
+							/>
+						</Form.Group>
+
+						{this.makeQuestions(this.state.questions)}
+						<Button
+							style={{ backgroundColor: '#2B96B7', border: '#2B96B7' }}
+							type="submit"
+							variant="primary"
+							className="d-inline-block"
+							disabled={this.state.disabled}>
+							Send
+						</Button>
+
+						{this.state.emailSent === true && <p className="d-inline success-msg">Email sent!</p>}
+						{this.state.emailSent === false && <p className="d-inline error-msg">Hmm, try again</p>}
+					</Form>
 				</Row>
 			</Container>
 		);
