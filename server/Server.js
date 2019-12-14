8b767c6733ee03415b482d448ee0575dcd64fe5d
Vitor Tomazzi <vitor.c.tomazzi@gmail.com>
2019-12-14 09:25:09 -0500

    testing dep

--- server/Server.js
+++ server/Server.js
@@ -14,13 +14,30 @@ app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: false })); comes with express now
 app.use(cookieParser());
 
+// Express only serves static assets in production
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('self-eval-react/build'));
+}
+
 app.use(cors());
+// app.use((req, res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', '*');
+// 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// 	next();
+// });
+
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	res.header(
+	  "Access-Control-Allow-Origin",
+	  "http://<YOUR-APP-NAME>.herokuapp.com"
+	);
+	res.header(
+	  "Access-Control-Allow-Headers",
+	  "Origin, X-Requested-With, Content-Type, Accept"
+	);
 	next();
-});
+  });
 
 // mongo atlas instructions - ignore
 // const MongoClient = require('mongodb').MongoClient;
